11ce5df0ee266fd6d9ea42962568655ab22ef271
Oliver Lipkau <oliver@lipkau.net>
2026-05-25 23:06:19 +0200

    Serialize sprint state names in update payloads

--- JiraAgilePS/Public/Set-Sprint.ps1
+++ JiraAgilePS/Public/Set-Sprint.ps1
@@ -53,14 +53,14 @@ function Set-Sprint {
 
             $body = @{ }
             if ($null -ne $currentSprint.Name) { $body['name'] = $currentSprint.Name }
-            if ($null -ne $currentSprint.State) { $body['state'] = $currentSprint.State }
+            if ($null -ne $currentSprint.State) { $body['state'] = $currentSprint.State.ToString() }
             if ($null -ne $currentSprint.StartDate) { $body['startDate'] = $currentSprint.StartDate }
             if ($null -ne $currentSprint.EndDate) { $body['endDate'] = $currentSprint.EndDate }
             if ($currentSprint.OriginBoardId -ne 0) { $body['originBoardId'] = $currentSprint.OriginBoardId }
             if ($null -ne $currentSprint.Goal) { $body['goal'] = $currentSprint.Goal }
 
             if ($PSBoundParameters.ContainsKey('Name')) { $body['name'] = $Name }
-            if ($PSBoundParameters.ContainsKey('State')) { $body['state'] = $State }
+            if ($PSBoundParameters.ContainsKey('State')) { $body['state'] = $State.ToString() }
             if ($PSBoundParameters.ContainsKey('StartDate')) { $body['startDate'] = $StartDate }
             if ($PSBoundParameters.ContainsKey('EndDate')) { $body['endDate'] = $EndDate }
             if ($PSBoundParameters.ContainsKey('Goal')) { $body['goal'] = $Goal }
